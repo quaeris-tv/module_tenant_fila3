7ca64744868a4543065042dfb10de7f2d1952728
marco76tv <marco.sottana@gmail.com>
2025-01-17 14:54:41 +0100

    up

--- app/Actions/Domains/GetDomainsArrayAction.php
+++ app/Actions/Domains/GetDomainsArrayAction.php
@@ -31,7 +31,7 @@ class GetDomainsArrayAction
 
     public function recurse(string $path): array
     {
-        $filesystem = new Filesystem;
+        $filesystem = new Filesystem();
         $directories = $filesystem->directories($path);
         $res = [];
         foreach ($directories as $dir) {
