da8ada544388501adb4c9301b6d81a49df197b2d
marco76tv <marco.sottana@gmail.com>
2025-02-04 13:47:47 +0100

    up

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -9,18 +9,20 @@ use Modules\Tenant\Actions\Domains\GetDomainsArrayAction;
 use Sushi\Sushi;
 
 /**
- * 
- *
  * @property int|null $id
  * @property string|null $name
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|Domain newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Domain newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Domain query()
  * @method static \Illuminate\Database\Eloquent\Builder|Domain whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Domain whereName($value)
+ *
  * @property-read \Modules\Broker\Models\Profile|null $creator
  * @property-read \Modules\Broker\Models\Profile|null $updater
+ *
  * @method static \Modules\Tenant\Database\Factories\DomainFactory factory($count = null, $state = [])
+ *
  * @mixin \Eloquent
  */
 class Domain extends BaseModel
