cba832dfa21cff816c3465eadb803633a3f73da6
marco76tv <marco.sottana@gmail.com>
2025-02-11 08:33:00 +0100

    up

---
+++ App/Filament/Resources/DomainResource/Pages/EditDomain.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Tenant\Filament\Resources\DomainResource\Pages;
+
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+use Modules\Tenant\Filament\Resources\DomainResource;
+
+class EditDomain extends EditRecord
+{
+    protected static string $resource = DomainResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
