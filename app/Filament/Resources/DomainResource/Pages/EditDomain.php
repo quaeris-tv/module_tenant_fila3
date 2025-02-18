434879ffe638a89b70778349b782b74b6ad7a7a0
marco76tv <marco.sottana@gmail.com>
2025-02-18 21:46:01 +0100

    Auto update submodule

--- app/Filament/Resources/DomainResource/Pages/EditDomain.php
+++ app/Filament/Resources/DomainResource/Pages/EditDomain.php
@@ -11,10 +11,5 @@ class EditDomain extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
 {
     protected static string $resource = DomainResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
-    }
+   
 }
