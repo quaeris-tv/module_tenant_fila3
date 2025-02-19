11212e74b4176a70eda6154b508942f0aca2fc8b
marco76tv <marco.sottana@gmail.com>
2025-02-19 19:01:13 +0100

    up

--- app/Filament/Resources/DomainResource/Pages/CreateDomain.php
+++ app/Filament/Resources/DomainResource/Pages/CreateDomain.php
@@ -7,7 +7,7 @@ namespace Modules\Tenant\Filament\Resources\DomainResource\Pages;
 use Filament\Resources\Pages\CreateRecord;
 use Modules\Tenant\Filament\Resources\DomainResource;
 
-class CreateDomain extends CreateRecord
+class CreateDomain extends \Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord
 {
     protected static string $resource = DomainResource::class;
 }
