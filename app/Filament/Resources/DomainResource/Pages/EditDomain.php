fc79fde3cc0aaaeec8cd5f68171dc06541acd868
marco76tv <marco.sottana@gmail.com>
2025-02-26 15:45:29 +0100

    Auto update submodule

--- app/Filament/Resources/DomainResource/Pages/EditDomain.php
+++ app/Filament/Resources/DomainResource/Pages/EditDomain.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Filament\Resources\DomainResource\Pages;
 
-use Filament\Actions;
 use Modules\Tenant\Filament\Resources\DomainResource;
 
 class EditDomain extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
 {
     protected static string $resource = DomainResource::class;
-
-   
 }
